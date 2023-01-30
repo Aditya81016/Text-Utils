6331d676a39bcf89897579656e472759f44213e8
Aditya81016 <115473507+Aditya81016@users.noreply.github.com>
2023-01-31 00:08:21 +0530

    Late Night 31st jan

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
+  <div class="container my-3">
     <h1>About</h1>
+    <p class="imp">This project is inspired by the Text Utils website from <a href="#">Code With Harry</a>'s <a href="#">React Tutorial Series</a>.</p>
+    <p>Unlike the tutorial this project is made in Vue, a reactive js framework like React.</p>
+    <p>Repository: <a href="#">Text-Utils GitHub</a></p>
+    <p>Creator: <a href="#">Aditya81016</a></p>
+    <p>Gmail: <a href="adi.jaiswal81016@gmail.com">adi.jaiswal81016@gmail.com</a></p>
   </div>
 </template>
 
@@ -10,4 +15,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,13 @@
   <div class="container my-3">
     <h1>Textarea</h1>
     <div class="form-floating my-3">
-      <textarea class="form-control" id="textarea" v-model="text" style="height: 50vh" placeholder="###"></textarea>
+      <textarea
+        class="form-control"
+        id="textarea"
+        v-model="text"
+        style="height: 50vh"
+        placeholder="###"
+      ></textarea>
       <label id="label" for="textarea">Enter your text here...</label>
     </div>
     <div class="containe d-flex justify-content-between">
@@ -15,16 +21,23 @@
       </div>
       <div class="d-flex gap-0">
         <span @click="removeSpace">
-          <butt text='<img src="src\icons\backspace-icon.svg">' title="Extra Space" content="remove extra space"></butt>
+          <butt
+            text='<img src="src\icons\backspace-icon.svg">'
+            title="Extra Space"
+            content="remove extra space"
+          ></butt>
         </span>
         <span @click="copy">
-          <butt text='<img src="src\icons\copy.svg">' title="Copy Text" content="copy the text in the textarea"></butt>
+          <butt
+            text='<img src="src\icons\copy.svg">'
+            title="Copy Text"
+            content="copy the text in the textarea"
+          ></butt>
         </span>
       </div>
     </div>
     <h4>Preview</h4>
     <pre>{{ text }}</pre>
-
   </div>
 </template>
 
